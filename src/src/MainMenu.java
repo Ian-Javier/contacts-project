bf43c364576b07aa83f6dbb73ab3de7bf0d3d431
Ian Arellano <ian.g.arellano01@gmail.com>
2021-09-23 17:45:36 -0500

    modified a lot of the code and add option 3 to mainMenu

--- src/src/MainMenu.java
+++ src/src/MainMenu.java
@@ -1,14 +1,18 @@
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.*;
 
 public class MainMenu {
+	//data paths
 	Path dataFolder = Paths.get("src/src/data");
 	Path filePath = Paths.get("src/src/data", "contacts.txt");
 
-	public void readFileAndOutput (Path pathToFile) {
+	//list to write to text file
+	public List<String> stringContact = new ArrayList<>();
+
+	//display contacts in console when view contacts is called
+	public void outputContacts (Path pathToFile) {
 		List<String> linesInTheFile = new ArrayList<>();
 		try {
 			linesInTheFile = Files.readAllLines(pathToFile);
@@ -18,29 +22,28 @@ public class MainMenu {
 		System.out.println("Name  |  Phone number");
 		System.out.println("---------------");
 		for (String line : linesInTheFile){
-
 			System.out.println(Arrays.toString(line.split(",")));
 		}
 	}
 
-	public void writeContactsToFile(List<Contact> users){
-		StringBuilder sb = new StringBuilder();
+	//write contact objects and adds them to the list.
+	public void objectToList(Contact contact){
+		String stringToAdd = contact.getName() + ", " + contact.getNumber();
+		stringContact.add(stringToAdd);
+	}
 
-		for(Contact user:users){
-			sb.append(user.getName()).append(",");
-			sb.append(user.getNumber()).append("\n");
-		}
-		String commaDelimeterString=sb.toString();
-		if( commaDelimeterString.length() > 0 )
-			commaDelimeterString = commaDelimeterString.substring(0, commaDelimeterString.length() - 1);
-
-		try{
-			Files.write(filePath, Collections.singleton(commaDelimeterString));
-		} catch (IOException ioe){
-			ioe.printStackTrace();
+	//searches list for object specified
+	private void searchContact(String contact) throws IOException {
+		List<String> lines = Files.readAllLines(filePath);
+		for (String line : lines) {
+			if (line.contains(contact)) {
+				System.out.println(line);
+			}
 		}
 	}
-	public void mainMenu() {
+
+	//console menu
+	public void mainMenu() throws IOException {
 		System.out.println("Hello, what would you like to do today?");
 		System.out.println("1. View Contacts");
 		System.out.println("2. Add a new contact");
@@ -51,23 +54,44 @@ public class MainMenu {
 		Scanner sc = new Scanner(System.in);
 		int userRequest = sc.nextInt();
 		if (userRequest < 6) {
+			//option 1 view contacts
 			if (userRequest == 1) {
-				readFileAndOutput(filePath);
-				mainMenu();
-			} else if (userRequest == 2){
-				System.out.println("Okay, let's make a new contact! Enter the name first.");
-				String newContactName = sc.nextLine();
-				System.out.println("Okay, now enter the phone number without any spaces");
-				String newContactNumber = sc.next() + sc.nextLine();
-				Contact customContact = new Contact(newContactName, newContactNumber);
-				System.out.println("Your new contact has been added to the list.");
+				outputContacts(filePath);
+				System.out.println("");
 				mainMenu();
 			}
-			System.out.println("Thank you for using our app.");
-		} else if (userRequest >= 6){
+			//option 2 add a new contact
+			else if (userRequest == 2){
+				System.out.println("Okay, let's make a new contact! Enter the name first.");
+				String newContactName = sc.next() + sc.nextLine();
+				System.out.println("Okay, now enter the phone number without any spaces");
+				String newContactNumber = sc.nextLine();
+				Contact customContact = new Contact(newContactName, newContactNumber);
+				objectToList(customContact);
+				Files.write(filePath, stringContact, StandardOpenOption.APPEND);
+				System.out.println("Your new contact has been added to the list.");
+				System.out.println("");
+				mainMenu();
+			}
+			//option 3 search for a contact
+			else if (userRequest == 3){
+				System.out.println("Which contact would you like to search for?");
+				String userContactSearch = sc.next() + sc.nextLine();
+				searchContact(userContactSearch);
+				System.out.println("");
+				mainMenu();
+			}
+			//option 4 delete a contact
+			else if (userRequest == 4){
+				System.out.println("Which contact would you like to delete?");
+				String contactToDelete = sc.next() + sc.nextLine();
+			}
+		} else if (userRequest >= 6) {
 			System.out.println("You entered an invalid number choice. Please pick a correct number.");
+			System.out.println("");
 			mainMenu();
 		}
+//		System.out.println("Thank you for using our app.");
 
 	}
 
