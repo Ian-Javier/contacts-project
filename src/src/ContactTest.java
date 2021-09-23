602f3e138a76b5c5178280938817d18d80ace80d
Ian Arellano <ian.g.arellano01@gmail.com>
2021-09-23 14:45:22 -0500

    third commit. add option 2

--- src/src/ContactTest.java
+++ src/src/ContactTest.java
@@ -9,47 +9,30 @@ import java.util.List;
 
 public class ContactTest {
 	public static void main(String[] args) throws IOException {
-		Contact contact1 = new Contact("bob", "1234567890");
-		Contact contact2 = new Contact("bob", "1234567890");
-		Contact contact3 = new Contact("bob", "1234567890");
-//		String TestContact = String.valueOf(contact.getName());
-		MainMenu menu = new MainMenu();
-		menu.objectToString(contact2);
-		menu.objectToString(contact3);
-//		menu.mainMenu();
+		//data paths
 		Path dataFolder = Paths.get("src/src/data");
 		Path filePath = Paths.get("src/src/data", "contacts.txt");
-//		List<String> contactList = Arrays.asList(contact.getNumber());
-//		try{
-//			Files.write(filePath, contactList);
-//		} catch (IOException ioe){
-//			ioe.printStackTrace();
-//		}
-		List<Contact> users = new ArrayList<Contact>();
-		Contact user1 = new Contact ("1","Eric");
 
-		Contact user2 = new Contact ("2","John");
-		Contact user3 = new Contact ("3","Ram");
+		//instantiate main menu loop
+		MainMenu menu = new MainMenu();
 
+		//initial contact creation
+		Contact contact1 = new Contact("Jeff Davis", "2104444444");
+		Contact contact2 = new Contact("Jenny", "8675309");
+		Contact contact3 = new Contact("John Doe", "2105555355");
+
+		//moves contacts to list
+		List<Contact> users = new ArrayList<>();
 		users.add(contact1);
 		users.add(contact2);
 		users.add(contact3);
-		StringBuilder sb = new StringBuilder();
+		//Turn contacts into strings and adds them to file
+		menu.writeContactsToFile(users);
 
-		for(Contact user:users){
-			sb.append(user.getName()).append(",");
-			sb.append(user.getNumber()).append("\n");
-		}
-		String commaDelimeterString=sb.toString();
-		if( commaDelimeterString.length() > 0 )
-			commaDelimeterString = commaDelimeterString.substring(0, commaDelimeterString.length() - 1);
+		//displays main menu
+		menu.mainMenu();
 
 
-		try{
-			Files.write(filePath, Collections.singleton(commaDelimeterString));
-		} catch (IOException ioe){
-			ioe.printStackTrace();
-		}
 
 
 	}
--- src/src/MainMenu.java
+++ src/src/MainMenu.java
@@ -2,10 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class MainMenu {
 	Path dataFolder = Paths.get("src/src/data");
@@ -18,8 +15,29 @@ public class MainMenu {
 		} catch (IOException ioe){
 			ioe.printStackTrace();
 		}
+		System.out.println("Name  |  Phone number");
+		System.out.println("---------------");
 		for (String line : linesInTheFile){
-			System.out.println(line);
+
+			System.out.println(Arrays.toString(line.split(",")));
+		}
+	}
+
+	public void writeContactsToFile(List<Contact> users){
+		StringBuilder sb = new StringBuilder();
+
+		for(Contact user:users){
+			sb.append(user.getName()).append(",");
+			sb.append(user.getNumber()).append("\n");
+		}
+		String commaDelimeterString=sb.toString();
+		if( commaDelimeterString.length() > 0 )
+			commaDelimeterString = commaDelimeterString.substring(0, commaDelimeterString.length() - 1);
+
+		try{
+			Files.write(filePath, Collections.singleton(commaDelimeterString));
+		} catch (IOException ioe){
+			ioe.printStackTrace();
 		}
 	}
 	public void mainMenu() {
@@ -33,10 +51,17 @@ public class MainMenu {
 		Scanner sc = new Scanner(System.in);
 		int userRequest = sc.nextInt();
 		if (userRequest < 6) {
-			while (userRequest != 5) {
-				if (userRequest == 1) {
-					readFileAndOutput(filePath);
-				}
+			if (userRequest == 1) {
+				readFileAndOutput(filePath);
+				mainMenu();
+			} else if (userRequest == 2){
+				System.out.println("Okay, let's make a new contact! Enter the name first.");
+				String newContactName = sc.nextLine();
+				System.out.println("Okay, now enter the phone number without any spaces");
+				String newContactNumber = sc.next() + sc.nextLine();
+				Contact customContact = new Contact(newContactName, newContactNumber);
+				System.out.println("Your new contact has been added to the list.");
+				mainMenu();
 			}
 			System.out.println("Thank you for using our app.");
 		} else if (userRequest >= 6){
@@ -45,13 +70,5 @@ public class MainMenu {
 		}
 
 	}
-	public void objectToString(Contact contact){
 
-		List<String> contactList = Arrays.asList(contact.getNumber());
-		try{
-			Files.write(filePath, contactList);
-		} catch (IOException ioe){
-			ioe.printStackTrace();
-		}
-	}
 }
